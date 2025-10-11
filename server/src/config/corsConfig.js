e1217c4f11a2a9c29347198202ab7201c1a177cb
Shubham9528 <81708211+Shubham9528@users.noreply.github.com>
2025-10-11 19:28:11 +0530

    chore: update CORS origins and add dist to gitignore

--- server/src/config/corsConfig.js
+++ server/src/config/corsConfig.js
@@ -1,11 +1,11 @@
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:3000", "http://localhost:3001","https://real-time-whiteboard-app.onrender.com"],
+  origin: ["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:3000", "http://localhost:3001","https://real-time-whiteboard-app-q5qu.vercel.app"],
   credentials: true,
   optionsSuccessStatus: 200
 };
 
 const socketCorsOptions = {
-  origin: ["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:3000", "http://localhost:3001","https://real-time-whiteboard-app.onrender.com"],
+  origin: ["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:3000", "http://localhost:3001","https://real-time-whiteboard-app-q5qu.vercel.app"],
   methods: ["GET", "POST"],
   credentials: true
 };
